61598cbcd1b5210c854763ef3ce70bcef37b8cf2
Dariana Barrientos <darianabarrientos09@gmail.com>
2024-04-09 18:36:23 -0300

    test: :fire: se elimino código de prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import '../styles/icons.css'
-
-import Icon from '../components/Icon'
-
-//no funciona sin esta linea a la primera
-
 export default function Home() {
 	return (
 		<div>
 			<h1>Landing Page</h1>
-			<Icon name='cat' size='80px' />
 		</div>
 	)
 }
