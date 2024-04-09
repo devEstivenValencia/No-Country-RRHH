3e3f5edcf76f8105433c8a1cd1fb8efcd1dae5a0
Yago <104085125+YagoGomez83@users.noreply.github.com>
2024-04-09 20:30:58 -0300

    Se creo Validacion y creacion de user y company vinculado a ese usuario

--- API/Laravel-Api/app/Http/Controllers/SessionController.php
+++ API/Laravel-Api/app/Http/Controllers/SessionController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Company;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\SessionRequest;
 
 class SessionController extends Controller
 {
-    public function store(Request $request)
+    public function store(SessionRequest $request)
     {
-        //
+        $data = $request->validated();
+        $user = User::create(['email' => $data['email'], 'password' => Hash::make($data['password']), 'disabled' => false]);
+
+
+        $company = new Company();
+        $company->id = Str::uuid();
+        $company->user_id = $user->id;
+        $company->save();
+
+        return response()->json(
+            ['token' => $user->createToken('token')->plainTextToken, 'user' => $user],
+            201
+        );
     }
 
     public function validate_token()
--- API/Laravel-Api/app/Http/Requests/SessionRequest.php
+++ API/Laravel-Api/app/Http/Requests/SessionRequest.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Foundation\Http\FormRequest;
 
 class SessionRequest extends FormRequest
 {
+    // protected function prepareForValidation()
+    // {
+    //     $this->merge([
+    //         'email' => Crypt::decryptString($this->email),
+    //         'password' => Crypt::decryptString($this->password),
+    //     ]);
+    // }
+
     public function authorize(): bool
     {
         return true;
@@ -14,7 +24,27 @@ class SessionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email' => 'required|email|unique:users,email',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'confirmed',
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/'
+            ],
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'email.required' => 'El email es obligatorio',
+            'email.email' => 'El email ingresado no es un email válido',
+            'email.unique' => 'El email ya ha sido registrado',
+            'email.exists' => 'El email no está registrado',
+            'password.required' => 'El password es obligatorio',
+            'password.min' => 'La contraseña debe tener al menos :min caracteres',
+            'password.regex' => 'La contraseña debe contener al menos una letra minúscula, una letra mayúscula, un dígito, un carácter especial y tener una longitud mínima de 8 caracteres.',
+            'password.confirmed' => 'La confirmación de contraseña no coincide'
         ];
     }
 }
