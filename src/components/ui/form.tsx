883bca12f0d44711e8c1b0d4360945aea82c87e9
devEstivenValencia <dev@estivenvalencia.co>
2024-04-12 11:33:54 -0500

    feat: 💄 comportamiento flex de FormItem

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -64,7 +64,7 @@ const FormItem = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivEl
 
 		return (
 			<FormItemContext.Provider value={{ id }}>
-				<div ref={ref} className={cn('space-y-2', className)} {...props} />
+				<div ref={ref} className={cn('flex flex-col gap-1.5', className)} {...props} />
 			</FormItemContext.Provider>
 		)
 	}
