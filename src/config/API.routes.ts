1eb1f7abd50c08bc1812086990f308f8be11adb4
devEstivenValencia <dev@estivenvalencia.co>
2024-04-12 11:32:18 -0500

    feat: ✨ ruta de Login de API y URL del server

--- src/config/API.routes.ts
+++ src/config/API.routes.ts
@@ -1,3 +1,7 @@
 import { KeyWithValue } from '#/types/KeyWithValue.type'
 
-export const APIROUTES: KeyWithValue<string> = {} as const
+const APIURL = process.env.APIURL || 'https://test-laravel-api1.000webhostapp.com/api'
+
+export const APIROUTES: KeyWithValue<string> = {
+	LOGIN: `${APIURL}/session/login`
+} as const
