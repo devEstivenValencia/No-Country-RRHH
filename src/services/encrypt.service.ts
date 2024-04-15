e44af0b9fa2d6103e62b2935813feada855dfc8d
devEstivenValencia <dev@estivenvalencia.co>
2024-04-14 21:51:53 -0500

    feat: ✨ servicio de encriptacion

---
+++ src/services/encrypt.service.ts
@@ -0,0 +1,13 @@
+'use server'
+
+import { APIROUTES } from '#/config/API.routes'
+import axios from 'axios'
+
+export async function encryptService<T>(values: T): Promise<T | undefined> {
+	try {
+		const { data } = await axios.post(APIROUTES.ENCRYPT, values)
+		return data
+	} catch (reason) {
+		console.error(reason)
+	}
+}
