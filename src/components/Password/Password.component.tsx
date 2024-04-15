02b92d85e7f2a0a17503c1ced188bd971730b6ec
devEstivenValencia <dev@estivenvalencia.co>
2024-04-15 11:55:16 -0500

    refactor: ♻️ ahora siempre se ejecutara el onChange pasado por Props

--- src/components/Password/Password.component.tsx
+++ src/components/Password/Password.component.tsx
@@ -48,7 +48,9 @@ export function Password({ variant, ...props }: PasswordProps) {
 	function onChange(e: React.ChangeEvent<HTMLInputElement>) {
 		if (variant === 'new-password') {
 			validateNewPassword(e)
-		} else if (props.onChange) props.onChange(e)
+		}
+
+		if (props.onChange) props.onChange(e)
 	}
 
 	return (
@@ -67,12 +69,10 @@ export function Password({ variant, ...props }: PasswordProps) {
 
 Password.Input = function PasswordInput({ onChange, ...props }: InputProps) {
 	const { shown, toggleShown, variant } = useContext(PasswordContext)
-
 	return (
 		<FormControl>
 			<div className='relative w-auto h-fit'>
 				<Input
-					{...props}
 					type={shown ? 'text' : 'password'}
 					id='password'
 					placeholder='Ejemplo123!'
@@ -80,6 +80,7 @@ Password.Input = function PasswordInput({ onChange, ...props }: InputProps) {
 					className='md:w-[448px]'
 					required
 					autoComplete={variant}
+					{...props}
 				/>
 				<button type='button' onClick={toggleShown} className='absolute top-2 right-3'>
 					<Icon name={shown ? 'eye' : 'eye-crossed'} />
@@ -110,16 +111,27 @@ Password.Validations = function PasswordValidations() {
 		number: 'Número [0...9]'
 	}
 
+	const listOfValidationsValues = Object.values(validations)
+	const trueValidations = listOfValidationsValues.filter(validation => validation)
+	const trueValidationCount = trueValidations.length
+
 	const validationComponents = Object.entries(validations).map(([key, isValid]) => (
 		<Validation key={key} text={validationTexts[key]} isValid={isValid} />
 	))
 
 	return (
-		<div className='flex flex-col gap-1.5 p-3 rounded-md shadow'>
-			<Typography as='p' className='text-xs'>
-				La contraseña debe cumplir los siguientes requisitos:
-			</Typography>
-			{validationComponents}
-		</div>
+		<>
+			<div className='flex w-full'>
+				<div className='bg-red-500 h-1.5 w-full rounded-full'></div>
+				<div className={cn('h-1.5 w-full rounded-full', trueValidationCount >= 2 && 'bg-yellow-500')}></div>
+				<div className={cn('h-1.5 w-full rounded-full', trueValidationCount === 4 && 'bg-green-500')}></div>
+			</div>
+			<div className='flex flex-col gap-1.5 p-3 rounded-md shadow'>
+				<Typography as='p' className='text-xs'>
+					La contraseña debe cumplir los siguientes requisitos:
+				</Typography>
+				{validationComponents}
+			</div>
+		</>
 	)
 }
