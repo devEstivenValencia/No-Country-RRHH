6e227323c02ed2bc32d5434d546ac2a1cc3e6598
devEstivenValencia <dev@estivenvalencia.co>
2024-04-17 12:46:35 -0500

    fix: 🔥 eliminado console.log innecesarios

--- src/services/encrypt.service.ts
+++ src/services/encrypt.service.ts
@@ -4,10 +4,8 @@ import { APIROUTES } from '#/config/API.routes'
 import axios from 'axios'
 
 export async function encryptService<T>(values: T): Promise<T | undefined> {
-	console.log('Valores', values)
 	try {
 		const { data } = await axios.post(APIROUTES.ENCRYPT, values)
-		console.log(data)
 		return data
 	} catch (reason: any) {
 		const { message } = reason.response.data || reason
